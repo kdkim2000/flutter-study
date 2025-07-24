cec58c57f078fddc48f1da615067edb906354f45
kdkim2000 <kdkim2000@gmail.com>
2025-07-24 10:01:21 +0900

    Create ch14_2_1_navigator.dart

---
+++ flutter_study/lib/chapter_doit/ch14_2_1_navigator.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  bool _isDeepLink = true;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigator Test',
+      home: Navigator(
+        pages: [
+          MaterialPage(child: OneScreen()),
+          if(_isDeepLink) MaterialPage(child: TwoScreen()),
+        ],
+        onPopPage: (route, result) => route.didPop(result),
+      ),
+    );
+  }
+}
+
+class OneScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('OneScreen'),),
+      body: Container(
+        color: Colors.red,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('OneScreen', style: TextStyle(color: Colors.white, fontSize: 30),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+class TwoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('TwoScreen'),),
+      body: Container(
+        color: Colors.red,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('TwoScreen', style: TextStyle(color: Colors.white, fontSize: 30),),
+              ElevatedButton(
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                  child: Text('Pop'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
