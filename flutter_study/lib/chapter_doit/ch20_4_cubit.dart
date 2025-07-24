eec116a5268edebd8295ee6ac289e8620d553624
kdkim2000 <kdkim2000@gmail.com>
2025-07-24 11:15:38 +0900

    Create ch20_4_cubit.dart

---
+++ flutter_study/lib/chapter_doit/ch20_4_cubit.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit(): super(0);
+  void increment() => emit(state +1);
+  void decrement() => emit(state -1);
+}
+
+main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Cubit Test'),),
+        body: BlocProvider(
+          create: (_) => CounterCubit(),
+          child: MyWidget(),
+        ),
+      ),
+    );
+  }
+}
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var cubit = BlocProvider.of<CounterCubit>(context);
+    return BlocBuilder<CounterCubit, int>(
+      builder: (context, count){
+        return Container(
+          color: Colors.deepOrange,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Cubit : $count',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: (){
+                      cubit.increment();
+                    },
+                    child: Text('increment'),
+                ),
+                ElevatedButton(
+                  onPressed: (){
+                    cubit.decrement();
+                  },
+                  child: Text('decrement'),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
