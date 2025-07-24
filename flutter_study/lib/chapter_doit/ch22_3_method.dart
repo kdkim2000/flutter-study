78aa7fa99480228dad1b6fc93fffd8b8f02da8cf
kdkim2000 <kdkim2000@gmail.com>
2025-07-24 11:27:17 +0900

    Create ch22_3_method.dart

---
+++ flutter_study/lib/chapter_doit/ch22_3_method.dart
@@ -0,0 +1,85 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Channel',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NativeCallWidget(),
+    );
+  }
+}
+class NativeCallWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return NativeCallWidgetState();
+  }
+}
+class NativeCallWidgetState extends State<NativeCallWidget>{
+  String? resultMessage;
+  String? receiveMessage;
+
+  Future<Null> nativeCall() async {
+    const channel = const MethodChannel('myMethodChannel');
+    try{
+      var details = {'Username': 'kkang', 'Password':'1234'};
+      final Map result = await channel.invokeMethod("oneMethod", details);
+      setState(() {
+        resultMessage = "${result['one']}, ${result['two']}";
+      });
+      channel.setMethodCallHandler((call) async {
+        switch (call.method){
+          case 'twoMethod':
+            setState(() {
+              receiveMessage = 'receive : ${call.arguments}';
+            });
+            return 'Reply from Dart';
+        }
+      });
+    }on PlatformException catch(e){
+      print('failed: ${e.message}');
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Method Channel'),),
+      body: Container(
+        color: Colors.deepPurpleAccent,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('resultMssage: $resultMessage', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+              Text('receiveMssage: $receiveMessage', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+              ElevatedButton(
+                onPressed: nativeCall,
+                child: Text('native call'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
